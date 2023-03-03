f19f566ec5f37f86036e2bfbb0b59b92c0b7f39f
kstathop <kostasstathopoulos99@gmail.com>
2023-03-03 12:21:54 +0200

    Add frontend implementation

--- movies/src/main/java/com/example/movies/MoviesApplication.java
+++ movies/src/main/java/com/example/movies/MoviesApplication.java
@@ -2,7 +2,7 @@ package com.example.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
 
 @SpringBootApplication
 public class MoviesApplication {
